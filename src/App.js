bee3a72ee7f2257d7609569bad6173df82abcdc1
Cesar Morales <cesarlenin2@live.com>
2020-04-06 12:17:56 -0700

    detach store fixed store.list undefine issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import List from './List.js';
 
 
 class App extends React.Component  {
@@ -8,19 +8,21 @@ class App extends React.Component  {
     store : {
     lists: [],
     allCards: {}
-  }}
+  }
+};
 
   render() {
-    const store =this.props
+    const {store} = this.props
     console.log(store)
+    console.log(store.lists)
   return (
     <main className='App'>
       <header className="App-header">
         <h1>Trelloyes!</h1>
       </header>
       <div className= "App-list">
-        {store.list.map(list => 
-        <list 
+        {store.lists.map(list => 
+        <List 
         key= {list.id}
         header= {list.header}
         cards={list.cardIds.map(id => store.allCards[id])}
---
+++ src/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function List(props){
+    return(
+        <h1></h1>
+    );
+
+}
+
+export default List;
