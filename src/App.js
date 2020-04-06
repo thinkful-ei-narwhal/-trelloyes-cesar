cb37d71e320d1397258aae35dd5a81316f9065a5
Cesar Morales <cesarlenin2@live.com>
2020-04-06 11:37:45 -0700

    added elements and props to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+
+
+class App extends React.Component  {
+  static defaultProps= {
+    store : {
+    lists: [],
+    allCards: {}
+  }}
+
+  render() {
+    const store =this.props
+    console.log(store)
   return (
     <main className='App'>
-      {/* content goes here */}
+      <header className="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className= "App-list">
+        {store.list.map(list => 
+        <list 
+        key= {list.id}
+        header= {list.header}
+        cards={list.cardIds.map(id => store.allCards[id])}
+        />
+        )}
+      </div>
     </main>
   );
+  }
 }
 
 export default App;
